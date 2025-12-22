1c042ef04691a2c94821668b67aa8be3816ae0aa
SandyAzzat <sandy.azzat.youssef@gmail.com>
2025-12-22 10:53:47 +0200

    Fix register page

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -10,7 +10,7 @@ import * as Yup from "yup";
 import { register } from "../../apis/Auth/register.api";
 import { toast } from "react-toastify";
 import { useTranslation } from "react-i18next";
-import registerBg from "../../assets/images/register.jpg";
+import registerBg from "../../assets/images/loginBg.webp";
 
 
 const locationData = {
